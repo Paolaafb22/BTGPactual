dff82e43e27447de03e7b0f7b90e163bd9055bb4
Paolaafb22 <p.forerob@uniandes.edu.co>
2025-07-13 23:42:06 -0500

    Agrega modelo de usuario y conexión a MongoDB

---
+++ app/core/database.py
@@ -0,0 +1,14 @@
+# app/core/database.py
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+client = MongoClient(MONGO_URI)
+
+db = client["fondos_btg"]  # Nombre de la base de datos
+users_collection = db["users"]
+funds_collection = db["funds"]
+transactions_collection = db["transactions"]
